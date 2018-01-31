7c25387515dec3a475c1dc2ee6ec70d0ec2a5eb6
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2018-01-31 11:34:05 +0700

    init(*): spring boot microservice tutorial part 2: forex-service
    https://dzone.com/articles/microservices-with-spring-boot-part-2-creating-a-f

---
+++ src/main/java/io/github/vazh/microservice/forexservice/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package io.github.vazh.microservice.forexservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+    ExchangeValue findByFromAndTo(String from, String to);
+}
